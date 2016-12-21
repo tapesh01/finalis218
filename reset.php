45711f0ee3ab9ace205cb2432c11bcce65dabaa0
Tapesh Nagarwal <tn64@afsconnect2.njit.edu>
2016-12-20 22:38:46 -0500

    added reset password function

---
+++ reset.php
@@ -0,0 +1,124 @@
+<?php require('includes/config.php');
+
+//if logged in redirect to members page
+if( $user->is_logged_in() ){ header('Location: memberpage.php'); }
+
+//if form has been submitted do:
+if(isset($_POST['submit'])){
+
+	//email validation
+	if(!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)){
+	    $error[] = 'Please enter a valid email address';
+	} else {
+		$stmt = $db->prepare('SELECT email FROM members WHERE email = :email');
+		$stmt->execute(array(':email' => $_POST['email']));
+		$row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+		if(empty($row['email'])){
+			$error[] = 'Email provided is not an existing user.';
+		}
+
+	}
+
+	//if no errors have been created carry on
+	if(!isset($error)){
+
+		//create the activasion code
+		$token = md5(uniqid(rand(),true));
+
+		try {
+
+			$stmt = $db->prepare("UPDATE members SET resetToken = :token, resetComplete='No' WHERE email = :email");
+			$stmt->execute(array(
+				':email' => $row['email'],
+				':token' => $token
+			));
+
+			//send email
+			$to = $row['email'];
+			$subject = "Password Reset";
+			$body = "<p>Someone requested that the password be reset.</p>
+			<p>If this was a mistake, just ignore this email and nothing will happen.</p>
+			<p>To reset your password, visit the following address: <a href='".DIR."resetPassword.php?key=$token'>".DIR."resetPassword.php?key=$token</a></p>";
+
+			$mail = new Mail();
+			$mail->setFrom(SITEEMAIL);
+			$mail->addAddress($to);
+			$mail->subject($subject);
+			$mail->body($body);
+			$mail->send();
+
+			//redirect to index page
+			header('Location: login.php?action=reset');
+			exit;
+
+		//else catch the exception and show the error.
+		} catch(PDOException $e) {
+		    $error[] = $e->getMessage();
+		}
+
+	}
+
+}
+
+//define page title
+$title = 'Reset Account';
+
+//include header template
+require('layout/header.php');
+?>
+<div id="MainHeader"> <!MainHeader div>
+<center>
+	<h1>Tapesh Nagarwal's Website</h1> <!centered header>
+</center>
+</div>
+<div class="container"> <!container div>
+
+	<div class="row">  <!row div>
+
+			<form role="form" method="post" action="" autocomplete="off">
+				<h2>Reset Password</h2>
+				<p><a href='login.php'>Back to login page</a></p>
+				<hr>
+
+				<?php
+				//check for any errors
+				if(isset($error)){
+					foreach($error as $error){
+						echo '<p class="bg-danger">'.$error.'</p>';
+					}
+				}
+
+				if(isset($_GET['action'])){
+
+					//check the action
+					switch ($_GET['action']) {
+						case 'active':
+							echo "<h2 class='bg-success'>Your account is now active you may now log in.</h2>";
+							break;
+						case 'reset':
+							echo "<h2 class='bg-success'>Please check your email for a reset link.</h2>";
+							break;
+					}
+				}
+				?>
+
+				<div class="form-group"> <!email div>
+					<input type="email" name="email" id="email" class="form-control input-lg" placeholder="Email" value="" tabindex="1">
+				</div>
+
+				<hr>
+				<div class="row"> <!row div>
+					<center><input type="submit" name="submit" value="Sent Reset Link" class="btn btn-primary btn-block btn-lg" tabindex="2"></center>
+				</div>
+			</form>
+		</div>
+	</div>
+
+
+</div>
+
+<?php
+//include header template
+require('layout/footer.php');
+?>
