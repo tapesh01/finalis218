9a81a73b07eb01a3a456dd918dbd5bf49bc9ce16
Tapesh Nagarwal <tn64@afsconnect2.njit.edu>
2016-12-20 22:44:13 -0500

    added the sql query

---
+++ db.sql
@@ -0,0 +1,17 @@
+/*
+sql query that creates a table called members
+has table values called memberID username password email active resetToken
+and resetComplete and the primary key is memberID
+*/
+
+
+CREATE TABLE `members` (
+  `memberID` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `active` varchar(255) NOT NULL,
+  `resetToken` varchar(255) DEFAULT NULL,
+  `resetComplete` varchar(3) DEFAULT 'No',
+  PRIMARY KEY (`memberID`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
